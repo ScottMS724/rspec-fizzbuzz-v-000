7c936f669bb264dbef1607bca18b2e55b72e7cff
Scott Silverman <scottms724@gmail.com>
2018-04-09 19:45:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(number)
-  if (number % 3 == 0)
-    return "Fizz"
+  if (number % 15 == 0)
+    return "FizzBuzz"
   elsif (number % 5 == 0)
     return "Buzz"
   elsif (number % 15 == 0)
